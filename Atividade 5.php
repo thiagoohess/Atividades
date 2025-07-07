c5b05e4c47b75a160482f5e2f25d9f1af61d9dea
Thiago <thiago_hess@estudante.sesisenai.org.br>
2025-07-07 12:11:47 -0300

    Atividade 5

---
+++ Atividade 5.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Número Amigo</title>
+</head>
+<body>
+    <h1>Não sei resolver</h1>
+</body>
+</html>
